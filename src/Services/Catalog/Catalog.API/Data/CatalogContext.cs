7f5a99809727b44048b089cc28b4110695e51ec4
younes reguieg <reguiegyounes@gmail.com>
2022-06-06 21:07:02 +0100

    Create catalog context with seed data

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var settings = configuration.GetValue<DatabaseSettings>("DatabaseSettings");
+            var client=new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            Products = database.GetCollection<Product>(settings.CollectionName);
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Data
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
