22c85efcf208c37b5ffcf784b1813ebc2cec9af8
younes reguieg <reguiegyounes@gmail.com>
2022-06-10 12:03:40 +0100

    Connect to redis docker

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -25,12 +25,15 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddStackExchangeRedisCache(options => {
+                options.Configuration=Configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
