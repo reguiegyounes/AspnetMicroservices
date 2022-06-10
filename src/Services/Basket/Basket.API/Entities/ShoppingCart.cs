146972fc198c653f406c522504f7ba033506db66
younes reguieg <reguiegyounes@gmail.com>
2022-06-10 11:47:40 +0100

    Add entities of basket

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public ShoppingCart(string userName)
+        {
+            this.UserName = userName;
+        }
+        public ShoppingCart()
+        {
+
+        }
+
+        public decimal TotalPrice 
+        {
+            get { 
+                decimal total = 0;
+                foreach (ShoppingCartItem item in Items) {
+                    total += item.Price * item.Quantity;
+                }
+                return total;
+            }
+        }
+    }
+}
