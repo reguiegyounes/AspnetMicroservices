0f3734d08dadae1fbbe157c12df8e6b2a1c6d8cc
younes reguieg <reguiegyounes@gmail.com>
2022-06-18 19:22:44 +0100

    Copy files and folders from discount.api to discount.grpc

---
+++ src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,81 @@
+﻿using Dapper;
+using Discount.Grpc.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            this._configuration = configuration ?? throw new ArgumentNullException(nameof(configuration)); ;
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            using var connection=new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                (
+                    "SELECT * FROM Coupon WHERE ProductName=@ProductName",
+                    new { ProductName=productName }
+                );
+            if (coupon == null)
+                return new Coupon() { 
+                    ProductName="No Discount",Amount=0,Description="No Discount Desc"
+                };
+
+            return coupon;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var effected = await connection.ExecuteAsync(
+                    @"INSERT INTO Coupon(ProductName,Amount,Description)
+                    VALUES(@ProductName,@Amount,@Description);",
+                    new {ProductName=coupon.ProductName,Amount=coupon.Amount,Description=coupon.Description }
+                );
+            if (effected == 0) return false;
+
+            return true;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var effected = await connection.ExecuteAsync(
+                    @"UPDATE Coupon SET 
+                        ProductName=@ProductName,
+                        Amount=@Amount,
+                        Description=@Description
+                    WHERE Id=@Id",
+                    new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description,Id=coupon.Id }
+                );
+            if (effected == 0) return false;
+
+            return true;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var effected = await connection.ExecuteAsync(
+                    @"DELETE FROM Coupon
+                        WHERE ProductName=@ProductName",
+                    new { ProductName = productName }
+                );
+            if (effected == 0) return false;
+
+            return true;
+        }
+
+        
+
+        
+    }
+}
--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Discount.Grpc.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Discount.Grpc
 {
@@ -16,6 +13,7 @@ namespace Discount.Grpc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
             services.AddGrpc();
         }
 
