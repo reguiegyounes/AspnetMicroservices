c38362386a2ea335a76d4e767a09f5b82f2a9c71
Younes Reguieg <reguiegyounes@gmail.com>
2022-06-30 16:57:09 +0100

    Ordering.Application Layer -create AddApplicationLayerServices  to collect all of its services

---
+++ src/Services/Ordering/Ordering.Application/Extensions/ApplicationLayerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviors;
+using System.Reflection;
+
+namespace Ordering.Application.Extensions
+{
+    public static class ApplicationLayerExtensions
+    {
+        public static IServiceCollection AddApplicationLayerServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
