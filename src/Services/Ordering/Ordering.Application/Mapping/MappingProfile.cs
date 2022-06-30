52e7d8e1076ec122604c5bfac5e5d59facba5eb7
Younes Reguieg <reguiegyounes@gmail.com>
2022-06-30 09:27:33 +0100

    Developing Ordering.Application Layer- Application Command Features- DeleteOrder

--- src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 using Ordering.Application.Features.Orders.Commands.UpdateOrder;
-using Ordering.Application.Features.Orders.Queries;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mapping
