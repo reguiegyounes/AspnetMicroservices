deddbaccf6cafca640bd7d526759ffec90ca6628
Younes Reguieg <reguiegyounes@gmail.com>
2022-07-13 21:37:23 +0100

    Developing BuildingBlocks EventBus.Messages Class Library

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedOn = DateTime.UtcNow;
+        }
+        public IntegrationBaseEvent(Guid id,DateTime createdOn)
+        {
+            Id = id;
+            CreatedOn = createdOn;
+        }
+         
+        public Guid Id { get;private set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
